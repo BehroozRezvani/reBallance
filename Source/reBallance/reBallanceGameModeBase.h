546afdee3a22a0158c9f04381fd80bfafd2aab99
BehroozRezvani <Brezvani@outlook.com>
2023-03-17 08:00:25 +0000

    Initial commit

---
+++ Source/reBallance/reBallanceGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "reBallanceGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class REBALLANCE_API AreBallanceGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
